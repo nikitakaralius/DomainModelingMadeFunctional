dbda0bba1aafc656935c7bc962a69193ae794916
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-19 13:15:23 +0300

    Created Simple types

---
+++ src/OrderTaking/Domain.fs
@@ -0,0 +1,21 @@
+namespace OrderTaking.Domain
+
+// constraint: starting with "W" then 4 digits
+type WidgetCode = WidgetCode of string
+
+// constraint: starting with "G" then 3 digits
+type GizmoCode = GizmoCode of string
+
+type ProductCode =
+    | Widget of WidgetCode
+    | Gizmo of GizmoCode
+
+type UnitQuantity = UnitQuantity of int
+
+// constraint: range(0.05, 1000.0)
+type KilogramQuantity = KilogramQuantity of decimal
+
+type OrderQuantity =
+    | Unit of UnitQuantity
+    | Kilos of KilogramQuantity
+
