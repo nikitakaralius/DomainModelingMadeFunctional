59b1218bbdfec0900260dcb565570f95cbc5c75f
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-19 16:16:36 +0300

    Reformat the file

--- src/OrderTaking/SimpleTypes.fs
+++ src/OrderTaking/SimpleTypes.fs
@@ -2,7 +2,6 @@ namespace OrderTaking.Domain
 
 open System
 open System.Text.RegularExpressions
-open Microsoft.FSharp.Core
 
 /// Constrained to be 50 chars or less, not null
 type String50 = private String50 of string
@@ -51,54 +50,72 @@ module ConstrainedType =
 
     let createString fieldName ctor maxLen str =
         if str |> String.IsNullOrEmpty then
-            let msg = sprintf "%s must not be null or empty" fieldName
+            let msg =
+                sprintf "%s must not be null or empty" fieldName
+
             Error msg
         elif str.Length > maxLen then
-            let msg = sprintf "%s must not be more than %i chars" fieldName maxLen
+            let msg =
+                sprintf "%s must not be more than %i chars" fieldName maxLen
+
             Error msg
         else
-            Ok (ctor str)
+            Ok(ctor str)
 
 
     let createStringOption fieldName ctor maxLen str =
         if str |> String.IsNullOrEmpty then
             Ok None
         elif str.Length > maxLen then
-            let msg = sprintf "%s must not be more than %i chars" fieldName maxLen
+            let msg =
+                sprintf "%s must not be more than %i chars" fieldName maxLen
+
             Error msg
         else
-            Ok (ctor str |> Some)
+            Ok(ctor str |> Some)
 
 
     let createInt fieldName ctor minVal maxVal x =
         if x < minVal then
-            let msg = sprintf "%s must not be less than %i" fieldName minVal
+            let msg =
+                sprintf "%s must not be less than %i" fieldName minVal
+
             Error msg
         elif x > maxVal then
-            let msg = sprintf "%s must not be greater than %i" fieldName maxVal
+            let msg =
+                sprintf "%s must not be greater than %i" fieldName maxVal
+
             Error msg
         else
-            Ok (ctor x)
+            Ok(ctor x)
 
     let createLike fieldName ctor pattern str =
-       if str |> String.IsNullOrEmpty then
-           let msg = sprintf "%s must not be null or empty" fieldName
-           Error msg
-       elif Regex.IsMatch(str, pattern) then
-           Ok (ctor str)
-       else
-           let msg = sprintf "%s: '%s' must match the pattern '%s'" fieldName str pattern
-           Error msg
+        if str |> String.IsNullOrEmpty then
+            let msg =
+                sprintf "%s must not be null or empty" fieldName
+
+            Error msg
+        elif Regex.IsMatch(str, pattern) then
+            Ok(ctor str)
+        else
+            let msg =
+                sprintf "%s: '%s' must match the pattern '%s'" fieldName str pattern
+
+            Error msg
 
     let createDecimal fieldName ctor minVal maxVal x =
         if x < minVal then
-            let msg = sprintf "%s must not be less than %M" fieldName minVal
+            let msg =
+                sprintf "%s must not be less than %M" fieldName minVal
+
             Error msg
         elif x > maxVal then
-            let msg = sprintf "%s must not be greater than %M" fieldName maxVal
+            let msg =
+                sprintf "%s must not be greater than %M" fieldName maxVal
+
             Error msg
         else
-            Ok (ctor x)
+            Ok(ctor x)
 
 
 module UnitQuantity =
